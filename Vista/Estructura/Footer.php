a13253516dc3bc50d61018a7218ac77323caf97b
DylanDuboscq <dylan.duboscq@est.fi.uncoma.edu.ar>
2025-11-10 10:48:20 -0300

    Footer.php añadido

---
+++ Vista/Estructura/Footer.php
@@ -0,0 +1,9 @@
+<footer class="">
+    <div class=""> 
+        <span>footer</span>
+    </div>
+</footer>
+
+<script src="<?php echo BASE_URL; ?>/Vista/Recursos/js/Validaciones.js"></script>
+</body>
+</html>
