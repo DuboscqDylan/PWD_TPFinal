f9749e456bd7533f7780ee9caeb3fd3d657587b8
DylanDuboscq <dylan.duboscq@est.fi.uncoma.edu.ar>
2025-11-10 10:30:23 -0300

    Mas estructura

--- Vista/Estructura/Header.php
+++ Vista/Estructura/Header.php
@@ -20,10 +20,36 @@ foreach($menues as $menu){
     }
 }
 
-$menuHtml .= "</div>"
+$menuHtml .= "</div>";
 
 if($sesionValida) {
-    $menuHtml .= "<a class='' href='".BASE_URL."/Vista/Paginas/Perfil/Perfil.php'>".$usuario->getUsnombre()."</a>";
+    $menuHtml .= "<a class='' href='".BASE_URL."/Vista/Paginas/Perfil/Perfil.php'>".$usuario->getUsnombre()."</a>
+    <a class='' href='".BASE_URL."/Vista/Paginas/Logout/Logout.php'>Logout</a>";
+}else{
+    $menuHtml .= "<a class='' href='".BASE_URL."/Vista/Paginas/Login/Login.php'>Login</a>
+    <a class='' href='".BASE_URL."/Vista/Paginas/Registro/Registro.php'>Registro</a>";
 }
 
+$menuHtml .= "</div>";
+
 ?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Programación web dinamica</title>
+
+    <link href="<?php echo BASE_URL; ?> /Vista/Recursos/css/bootstrap/bootstrap.min.css" rel="stylesheet">
+
+    <link href="<?php echo BASE_URL; ?> /Vista/Recursos/js/jquery-easyui-1.11.0/themes/bootstrap/easyui.css" rel="stylesheet">
+
+    <link href="<?php echo BASE_URL; ?> /Vista/Recursos/css/styles.css" rel="stylesheet">
+
+    <script src="<?php echo BASE_URL; ?>/Vista/Recursos/js/bootstrap/bootstrap.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>/Vista/Recursos/js/bootstrap/bootstrap.bundle.js"></script>
+</html>
+  
