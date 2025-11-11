8ae49ad2e7683daf3a4e00e1291b69a84d8bb1a9
DylanDuboscq <dylan.duboscq@est.fi.uncoma.edu.ar>
2025-11-11 10:53:47 -0300

    Vista producto

--- Vista/Paginas/Producto/Producto.php
+++ Vista/Paginas/Producto/Producto.php
@@ -11,9 +11,50 @@ if(!empty($data)){
         $producto = $arregloProductos[0];
         $rutaImagenes = BASE_URL."/Vista/Media/Producto/".$producto->getIdproducto();
 
-        $scan = glob($rutaImagenes.'/*.png');
-        if(!empty($scan)){
-            
+        $scanHeader = glob($rutaImagenes.'/*.png');
+        if(!empty($scanHeader)){
+            $header = "<img class='' src='".$scanHeader[0]."' height='250' alt='portada'>";
+        }else{
+            $header = '<p>No hay portada</p>';
         }
+
+        $scanDiv = glob($rutaImagenes."/Preview/*");
+        if(!empty($scanDiv)){
+            $previaDiv="";
+            foreach($scanDiv as $ruta) {
+                $previaDiv .= "<img class='' src='".$ruta."'>'";
+            }
+        }else{
+            $previaDiv = "<p class=''> No hay imagenes </p>";
+        }
+
+        $resultado = "<div class=''>
+                        <!---Portada--->
+                        <div class=''>
+                            <div class=''>
+                                <h1>".$producto->getPronombre()."</h1>
+                            </div>
+                            <div class=''>
+                            ".$header."
+                            </div>
+                        <div>
+                        <!---Detalle--->
+                        <div class=''>
+                            <p>".$producto->getProdetalle()."</p>
+                        </div>
+                        <div class=''>
+                        </div>
+                    </div>";
+    }else{
+        $resultado = "<h3> No se encontró el producto </h3>";
     }
+}else{
+    $resultado = "<h3> No se seleccionó un producto </h3>";
 }
+
+echo $resultado;
+
+include BASE_URL . "/Vista/Estructura/Footer.php";
+
+?>
+
