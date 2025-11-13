64216ecca51d8ea6df1f896bb0c317da5196c2db
mateo alday <aldaymateo1@gmail.com>
2025-11-13 10:36:55 -0300

    usuario

--- Vista/Paginas/Carrito/Accion/Vaciar.php
+++ Vista/Paginas/Carrito/Accion/Vaciar.php
@@ -2,6 +2,6 @@
 include_once $_SERVER['DOCUMENT_ROOT'] . "/PWD_TPFINAL/configuracion.php";
 $data = data_submitted();
 
-$respuesta = (new AbmCompraItem())->vaciarCarrito($data);
+$respuesta = (new ABMCompraItem())->vaciarCarrito($data);
 echo json_encode($respuesta);
 ?>
