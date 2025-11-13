c61ac581490ec04a22f6000227310db163ca6c0b
DylanDuboscq <dylan.duboscq@est.fi.uncoma.edu.ar>
2025-11-12 22:59:28 -0300

    Más correcciones de rutas

--- Vista/Paginas/Compras/Accion/ListarCompraEstados.php
+++ Vista/Paginas/Compras/Accion/ListarCompraEstados.php
@@ -1,5 +1,5 @@
 <?php
-include_once ROOT_PATH."/configuracion.php";
+include_once $_SERVER['DOCUMENT_ROOT']."/configuracion.php";
 $data = data_submitted();
 
 $salida = (new AbmCompraEstado())->listarCompraEstados($data); //['idcompraestadotipo']
