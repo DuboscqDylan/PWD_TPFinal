ee65330113fb2f00fbf6a9f2745e7cefcb9b1be8
DylanDuboscq <dylan.duboscq@est.fi.uncoma.edu.ar>
2025-11-18 21:43:47 -0300

    quité un var_dump

--- Vista/Paginas/Usuarios/Accion/HabilitarUsuario.php
+++ Vista/Paginas/Usuarios/Accion/HabilitarUsuario.php
@@ -1,7 +1,6 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT']."/PWD_TPFINAL/configuracion.php";
 $data = data_submitted();  // Obtener los datos enviados
-var_dump($_POST);
 $respuesta = (new AbmUsuario())->habilitarUsuario($data);
 
 echo json_encode($respuesta);
